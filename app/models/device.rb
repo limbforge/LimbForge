0cdb8be83016ea662bab199e3f76f98740a5c193
Kaitlyn Hova <kaitlynhova@gmail.com>
2016-08-27 12:19:04 -0700

    adding image upload to device icon

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,10 @@
 class Device < ActiveRecord::Base
   has_and_belongs_to_many :measurements
+  has_attached_file :icon, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :icon, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827183202) do
+ActiveRecord::Schema.define(version: 20160827190912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20160827183202) do
   create_table "devices", force: :cascade do |t|
     t.string   "name"
     t.string   "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "icon_file_name"
+    t.string   "icon_content_type"
+    t.integer  "icon_file_size"
+    t.datetime "icon_updated_at"
   end
 
   create_table "devices_measurements", id: false, force: :cascade do |t|
