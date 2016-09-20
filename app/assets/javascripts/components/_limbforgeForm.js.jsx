c53d733cf871e195d6e6a8899fe68af32edb3177
hovanem <matt.hovanec@autodesk.com>
2016-09-20 00:41:52 -0700

    now with one third the fun

--- app/assets/javascripts/components/_limbforgeForm.js.jsx
+++ app/assets/javascripts/components/_limbforgeForm.js.jsx
@@ -78,9 +78,12 @@ var LimbforgeForm = React.createClass({
       url: this.props.components_search_path + "?query="+event.target.value,
       dataType: 'json',
       success: function(data) {
-        this.setState({components: data});
-        this.setState({tds: undefined});
-        this.setState({measurements: undefined});
+        var newState = {
+          components: data,
+          tds: undefined,
+          measurements: undefined
+        };
+        this.setState(newState);
       }.bind(this),
       error: function(data) {
       }.bind(this)
@@ -100,13 +103,16 @@ var LimbforgeForm = React.createClass({
   getMeasurements: function(event) {
     var newSpecs = this.state.specs;
     newSpecs.component = event.target.value;
-    this.setState({specs: newSpecs});
     this.getTDs(event.target.value);
     $.ajax({
       url: this.props.measurements_search_path + "?query="+event.target.value,
       dataType: 'json',
       success: function(data) {
-        this.setState({measurements: data});
+        var newState = {
+          specs: newSpecs,
+          measurements: data,
+        }
+        this.setState(newState);
       }.bind(this),
       error: function(data) {
       }.bind(this)
@@ -119,13 +125,10 @@ var LimbforgeForm = React.createClass({
       var newSpecs = this.state.specs;
       newSpecs.orientation = event.target.value.charAt(0).toUpperCase();
       this.setState({specs: newSpecs});
-      scene.remove(scene.children[3]);
     }
     //if terminal devices selector changed
-    if (event.target.id == "terminal-devices-select"){
+    else if (event.target.id == "terminal-devices-select"){
       if (this.state.specs.TD != undefined || this.state.specs.TD != ""){
-        scene.remove(scene.children[4]);
-        scene.remove(scene.children[3]);
         var newSpecs = this.state.specs;
         newSpecs.TD = event.target.value;
         this.setState({specs: newSpecs});
@@ -137,7 +140,7 @@ var LimbforgeForm = React.createClass({
       }
     }
     //if L1 Changed
-    if (event.target.name == "L1") {
+    else if (event.target.name == "L1") {
       var L1Value = Number(event.target.value);
       var L1Measurements = this.state.measurements.find(function(measurement) {
         return measurement.name == "L1";
@@ -145,13 +148,12 @@ var LimbforgeForm = React.createClass({
       if (L1Measurements && L1Measurements.lower_range < L1Value && L1Measurements.upper_range > L1Value) {
         var newSpecs = this.state.specs;
         newSpecs.L1 = L1Value;
-        scene.remove(scene.children[3]);
         this.setState({specs: newSpecs});
       }
     }
 
     //if C4 Changed
-    if (event.target.name == "C4") {
+    else if (event.target.name == "C4") {
       var C4Value = Number(event.target.value);
       var C4Measurements = this.state.measurements.find(function(measurement) {
         return measurement.name == "C4";
@@ -159,7 +161,6 @@ var LimbforgeForm = React.createClass({
       if (C4Measurements && C4Measurements.lower_range < C4Value && C4Measurements.upper_range > C4Value) {
         var newSpecs = this.state.specs;
         newSpecs.C4 = C4Value;
-        scene.remove(scene.children[3]);
         this.setState({specs: newSpecs});
       }
     }
@@ -182,13 +183,11 @@ var LimbforgeForm = React.createClass({
     }
   },
   loadNewDevices: function(){
-    scene.remove(scene.children[3]);
-    scene.remove(scene.children[4]);
     var self = this;
 
     if (this.state.specs.component != undefined){
       // LOAD NEW devices
-      console.log("loading forearm");
+      scene.remove(scene.children[3]);
       loader.load( 'https://s3.amazonaws.com/limbforgestls/EbeArm/Ebe_forearm_' + this.state.specs.orientation + '/forearm_'+ this.state.specs.orientation + '_C4-'+ (this.state.specs.C4 *10) +'_L1-'+ (this.state.specs.L1 *10) + '.stl', function ( geometry ) {
         var mesh = new THREE.Mesh( geometry, material );
 
@@ -219,7 +218,7 @@ var LimbforgeForm = React.createClass({
     }
   },
   render: function() {
-    console.log(this.state.specs);
+    console.log(this.state.specs, scene.children.length);
     this.loadNewDevices();
     this.loadTD();
     var self = this;
