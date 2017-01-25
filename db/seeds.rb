40b4cbb1e1d44db1875ca8f121cf5e04162a7147
Kaitlyn Hova <kaitlynhova@gmail.com>
2017-01-24 21:38:50 -0800

    updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+amputationLevels = AmputationLevel.create(
+  [
+    { name: 'Shoulder Disarticulation' },
+    { name: 'Transhumeral' },
+    { name: 'Elbow Disarticulation' },
+    { name: 'Transradial' },
+    { name: 'Wrist Disarticulation' },
+    { name: 'Transcarpal' }
+  ]
+)
+
+measurements = Measurement.create(
+  [
+    { name: 'L1', step: 0.5, upper_range: 32, measurement_unit: "cm", lower_range: 18, default: 25, instructions: 'instructions go here' },
+    { name: 'L2', step: 0.5, upper_range: 28, measurement_unit: "cm", lower_range: 20, default: 25, instructions: 'instructions go here' },
+    { name: 'C1', step: 0.5, upper_range: 28, measurement_unit: "cm", lower_range: 20, default: 25, instructions: 'instructions go here' },
+    { name: 'C2', step: 0.5, upper_range: 28, measurement_unit: "cm", lower_range: 20, default: 25, instructions: 'instructions go here' },
+    { name: 'C3', step: 0.5, upper_range: 28, measurement_unit: "cm", lower_range: 20, default: 25, instructions: 'instructions go here' },
+    { name: 'C4', step: 0.5, upper_range: 28, measurement_unit: "cm", lower_range: 20, default: 25, instructions: 'instructions go here' }
+  ]
+)
+
+components = Component.create(
+  [
+    { name: 'EbeArm' },
+    { name: 'Hand' }
+  ]
+)
+
+terminal_devices = TerminalDevice.create(
+  [
+    { name: 'phone' },
+    { name: 'standard'}
+  ]
+)
+
+admin = User.create(
+  email:'admin@hovalabs.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true
+)
