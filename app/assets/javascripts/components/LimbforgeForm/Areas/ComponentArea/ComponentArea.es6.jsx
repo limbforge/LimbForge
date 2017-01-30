f77388ebd2b97454255e07d24971a11495cd2274
matthova <matthova87@gmail.com>
2017-01-30 00:05:46 -0800

    added toggleability to prosthesis aka components area

--- app/assets/javascripts/components/LimbforgeForm/Areas/ComponentArea/ComponentArea.es6.jsx
+++ app/assets/javascripts/components/LimbforgeForm/Areas/ComponentArea/ComponentArea.es6.jsx
@@ -3,8 +3,7 @@ class ComponentArea extends React.Component {
     super(props);
   }
 
-  render() {
-    var classes =  this.props.availableAreas.prosthesis.selected ? 'accordion-head active' : 'accordion-head';
+  renderComponents() {
     const componentOptions = [];
     if (this.props.components != undefined){
       this.props.components.map((option) => {
@@ -24,23 +23,28 @@ class ComponentArea extends React.Component {
         );
       });
     }
-    var components = <div></div>;
-    if (this.props.availableAreas.prosthesis.selected){
-      components = this.props.components == undefined ? "" :
-        <div className="row">
-          <div className="col-xs-12 tab-padding">
-            <p className="label">Select a Component</p>
-            {componentOptions}
-          </div>
-        </div>;
-    };
+
+    return (
+      <div className="row">
+        <div className="col-xs-12 tab-padding">
+          <p className="label">Select a Component</p>
+          {componentOptions}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    var classes =  this.props.availableAreas.prosthesis.selected ? 'accordion-head active' : 'accordion-head';
 
     return(
-      <div className={classes}>
-        <h2>Prosthesis</h2>
-        <span className="arrow"></span>
-        <span className="line"></span>
-        {components}
+      <div>
+        <div onClick={()=>this.props.updateSelectedArea('prosthesis')} className={classes}>
+          <h2>Prosthesis</h2>
+          <span className="arrow"></span>
+          <span className="line"></span>
+        </div>
+        {this.props.availableAreas.prosthesis.selected ? this.renderComponents() : ''}
       </div>
     )
   }
