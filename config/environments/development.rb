5b3131f7636920bd172ad36440241eb31b7bd6ae
Kaitlyn Hova <kaitlynhova@gmail.com>
2017-03-12 14:31:30 -0700

    FIX: action mailer password reset
    * also adding a quick “sign out” button to user dashboard

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,8 +23,20 @@ Rails.application.configure do
   #   }
   # }
 
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_mailer.delivery_method = :smtp
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
