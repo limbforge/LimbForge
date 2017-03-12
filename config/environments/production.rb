f599b5460aea4a787f409213eae5ad4e09416dca
Kaitlyn Hova <kaitlynhova@gmail.com>
2017-03-12 14:54:40 -0700

    FIX: password reset email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_mailer.delivery_method = :smtp
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
