2f2e848db1fc374137667cc3aed173a05516d242
Kaitlyn Hova <kaitlynhova@gmail.com>
2017-04-20 22:49:57 -0700

    removing binding.pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :host_name
-  binding.pry
   def host_name
     ActionMailer::Base.default_url_options = {:host => request.host_with_port}
   end
