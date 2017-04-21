47e67586f591a3e32615cc7b7781506ad9017abb
Kaitlyn Hova <kaitlynhova@gmail.com>
2017-04-20 22:29:07 -0700

    moving action mailer host method to before_filter in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+  before_filter :host_name
+  binding.pry
+  def host_name
+    ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+  end
 end
