3914647f9b19ff3b4a3b9ba77671089ab1bf326d
Kaitlyn Hova <kaitlynhova@gmail.com>
2017-10-26 18:55:40 -0700

    changing LimbForge capitalization in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,11 @@ class UserMailer < ApplicationMailer
 
   def request_access(user)
     @user = user
-    mail(to: 'learnmore@limbforge.org', subject: 'Limbforge app access request')
+    mail(to: 'learnmore@limbforge.org', subject: 'LimbForge app access request')
   end
 
   def access_granted(user)
     @user = user
-    mail(to: @user.email, subject: 'Limbforge app access granted!')
+    mail(to: @user.email, subject: 'LimbForge app access granted!')
   end
 end
